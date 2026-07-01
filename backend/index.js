859c5bbb0bc965dcb1bbe05ef00e4d7398455885
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-07-01 19:27:53 +0530

    full upadate

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,6 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -12,6 +14,72 @@ app.get("/login", (req, res) => {
 app.get("/signup", (req, res) => {
   res.send("Signup ");
 });
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+
+app.get("/jokes", (req, resp) => {
+  const jokes = [
+    {
+      id: 1,
+      setup: "Why don't programmers like nature?",
+      punchline: "It has too many bugs.",
+      category: "Programming",
+    },
+    {
+      id: 2,
+      setup: "Why did the Java developer wear glasses?",
+      punchline: "Because they couldn't C#.",
+      category: "Programming",
+    },
+    {
+      id: 3,
+      setup: "Why do Java developers prefer dark mode?",
+      punchline: "Because light attracts bugs.",
+      category: "Programming",
+    },
+    {
+      id: 4,
+      setup: "Why was the computer cold?",
+      punchline: "It left its Windows open.",
+      category: "Technology",
+    },
+    {
+      id: 5,
+      setup: "Why did the developer go broke?",
+      punchline: "Because they used up all their cache.",
+      category: "Programming",
+    },
+    {
+      id: 6,
+      setup: "How many programmers does it take to change a light bulb?",
+      punchline: "None. That's a hardware problem.",
+      category: "Programming",
+    },
+    {
+      id: 7,
+      setup: "Why don't skeletons fight each other?",
+      punchline: "They don't have the guts.",
+      category: "General",
+    },
+    {
+      id: 8,
+      setup: "What do you call fake spaghetti?",
+      punchline: "An impasta.",
+      category: "Food",
+    },
+    {
+      id: 9,
+      setup: "Why did the scarecrow win an award?",
+      punchline: "Because he was outstanding in his field.",
+      category: "General",
+    },
+    {
+      id: 10,
+      setup: "Why couldn't the bicycle stand up by itself?",
+      punchline: "Because it was two-tired.",
+      category: "General",
+    },
+  ];
+  res.json(jokes);
+});
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${process.env.PORT}...`);
 });
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios"; 
+import { useEffect } from "react";
 
 const App = () => {
-  return <div>App</div>;
+  const [joke, setJoke] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/signup")
+      .then((response) => {
+        setCount(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+
+  return (
+    <div>
+      <h1>HELLO WORLD!</h1>
+    </div>
+  );
 };
 
 export default App;
