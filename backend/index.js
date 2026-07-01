733446cbd4c43090756f5092ec68ef7620906653
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-07-02 01:14:10 +0530

    update the index.js

--- backend/index.js
+++ backend/index.js
@@ -80,6 +80,10 @@ app.get("/jokes", (req, resp) => {
   ];
   res.json(jokes);
 });
+
+
+
+
 app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port ${process.env.PORT}...`);
 });
