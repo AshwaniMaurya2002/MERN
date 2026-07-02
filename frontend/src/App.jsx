9f95bc8a70dcbfacca93d40af79a8dac3143a7d2
AshwaniMaurya2002 <ashwanikumarmaurya2002@gmail.com>
2026-07-02 15:04:59 +0530

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,27 @@
 import React from "react";
 import { useState } from "react";
-import axios from "axios"; 
+import axios from "axios";
 import { useEffect } from "react";
 
 const App = () => {
   const [joke, setJoke] = useState([]);
 
   useEffect(() => {
-    axios.get("http://localhost:4000/signup")
+    axios
+      .get("http://localhost:4000/signup")
       .then((response) => {
-        setCount(response.data);
+        setJoke(response.data);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
       });
 
-  return (
-    <div>
-      <h1>HELLO WORLD!</h1>
-    </div>
-  );
+    return (
+      <div>
+        <h1>HELLO WORLD!</h1>
+      </div>
+    );
+  });
 };
 
 export default App;
